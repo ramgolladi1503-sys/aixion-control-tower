b087a952388f0f3b0b9818a71e3aa655979cc2a0
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-05-07 04:21:21 +0530

    Add Compose color palette

---
+++ mobile/android/app/src/main/java/com/aixion/controltower/core/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.aixion.controltower.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TowerBackground = Color(0xFF070B12)
+val TowerSurface = Color(0xFF101826)
+val TowerSurfaceRaised = Color(0xFF172235)
+val TowerTextPrimary = Color(0xFFF3F7FB)
+val TowerTextMuted = Color(0xFF8C9AAF)
+val TowerAccent = Color(0xFF69E2FF)
+val RiskLow = Color(0xFF47D18C)
+val RiskMedium = Color(0xFFFFC857)
+val RiskHigh = Color(0xFFFF8A3D)
+val RiskCritical = Color(0xFFFF4D6D)
+val RiskBlocked = Color(0xFFB83DFF)
