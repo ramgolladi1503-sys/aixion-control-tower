0bbe84dc2c34cd764899b3e8e796be9a9812b7cc
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-05-08 23:12:04 +0530

    Add Android cache and auth dependencies

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.google.firebase:firebase-messaging:24.1.0")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.5")
 }
