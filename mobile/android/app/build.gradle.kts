5c2726b33d8057cd0ee340c73da9ea222f00b4b6
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-05-11 02:47:46 +0530

    Add coroutine test dependency for Android unit guards

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     implementation("com.google.firebase:firebase-messaging:24.1.0")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.5")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
 }
