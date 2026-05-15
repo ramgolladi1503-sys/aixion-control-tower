e2017bdd4c85343c2c242ac6698ed5684242dca6
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-05-15 20:01:23 +0530

    Add forged UI spacing tokens

---
+++ mobile/android/app/src/main/java/com/aixion/controltower/core/ui/theme/Spacing.kt
@@ -0,0 +1,20 @@
+package com.aixion.controltower.core.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object TowerSpacing {
+    val xs = 4.dp
+    val sm = 8.dp
+    val md = 12.dp
+    val lg = 16.dp
+    val xl = 20.dp
+    val xxl = 28.dp
+    val xxxl = 36.dp
+}
+
+object TowerRadius {
+    val sm = 12.dp
+    val md = 18.dp
+    val lg = 24.dp
+    val xl = 30.dp
+}
